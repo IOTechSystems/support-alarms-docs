4ceedca30b5f4a247ce6a0fee8367cb12e05fc70
Ali Almohammad <ali@iotechsys.com>
2024-10-25 08:36:18 +0100

    Renamed GraphQL Schema files

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -3,7 +3,7 @@
 # Generate Markdown file for GraphQL Scehma
 
 # removed unsupported directive "@specifiedBy"
-cat /graphql/router.graphqls /graphql/alarm_router.graphqls | sed 's/scalar JSON @specifiedBy(url: ".*")/scalar JSON/' > /graphql/support-alarms-schema.graphql
+cat /graphql/router-schema.graphqls /graphql/alarms-schema.graphqls | sed 's/scalar JSON @specifiedBy(url: ".*")/scalar JSON/' > /graphql/support-alarms-schema.graphql
 
 /usr/local/bin/graphql-markdown /graphql/support-alarms-schema.graphql > /graphql/support-alarms-schema.md
 rm /graphql/support-alarms-schema.graphql
